845e95eabddc3d213c12fce434e3d15eddc59421
borra-thirumala <thirumala2502@gmail.com>
2025-04-27 22:18:00 +0530

    first commit

---
+++ Strings/1Equalhalves.js
@@ -0,0 +1,13 @@
+// Medium:
+
+// 1.	Write a program that takes a string, string should be of even length. Divide the string into two equals parts, check the number of vowels in both the parts of the string. If both parts of string have same number of vowels then return true otherwise return false.
+
+// a.	Testcase1	:  rebels
+// b.	Output     	:  true
+// c.	Explanation 	:  Given sring rebels divided into two parts, reb and els. In both parts vowels count is same that is 1(e is presented in both the parts) so it returns true.
+
+// d.	Testcase2	:  apples
+// e.	Output     	:  true
+
+// f.	Testcase1	:  mars
+// g.	Output     	:  false
